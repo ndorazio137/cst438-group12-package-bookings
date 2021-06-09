cf473b9ae3dd77aca5267d6d9c5e373d685b90f5
kyleoakes <koakes@csumb.edu>
2021-06-09 03:13:09 -0700

    added postReservation method to CarService

--- src/main/java/cst438/service/CarService.java
+++ src/main/java/cst438/service/CarService.java
@@ -6,12 +6,19 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class CarService 
@@ -32,24 +39,48 @@ public class CarService
                restTemplate.getForEntity(
                      carUrl + "/" + cityName + "/" + startDate + "/" + endDate, 
                      JsonNode.class);
-            JsonNode json = response.getBody();
-            log.info("Status code from car server:" +
-                  response.getStatusCodeValue());
-            //TODO: finish when known api structure exists
-            return null;
+         JsonNode json = response.getBody();
+         log.info("Status code from car server:" +
+               response.getStatusCodeValue());
+         //TODO: finish when known api structure exists
+         return null;
       }
       
-      public List<Object> getReservationById(String reservationId) {
+      public Object getReservationById(String reservationId) {
          ResponseEntity<JsonNode> response =
                restTemplate.getForEntity(
                      carUrl + "/reservations/details/" + reservationId, 
                      JsonNode.class);
-            JsonNode json = response.getBody();
-            log.info("Status code from car server:" +
-                  response.getStatusCodeValue());
-            //TODO: finish when known api structure exists
-            return null;
+         JsonNode json = response.getBody();
+         log.info("Status code from car server:" +
+               response.getStatusCodeValue());
+         //TODO: finish when known api structure exists
+         return null;
       }
       
-      
+      public Object postReservation(String email, String carId, String dateStart, String dateEnd) throws JsonMappingException, JsonProcessingException {
+         
+         String postReservationUrl = carUrl + "/reserve";
+         
+         restTemplate = new RestTemplate();
+         HttpHeaders headers = new HttpHeaders();
+         headers.setContentType(MediaType.APPLICATION_JSON);
+         ObjectNode reservationJsonObject = new ObjectNode(null);
+         reservationJsonObject.put("email", email);
+         reservationJsonObject.put("car_id", carId);
+         reservationJsonObject.put("date_start", dateStart);
+         reservationJsonObject.put("date_end", dateEnd);
+         ObjectMapper objectMapper = new ObjectMapper();
+         HttpEntity<String> request = 
+               new HttpEntity<String>(reservationJsonObject.toString(), headers);
+             
+         ResponseEntity<String> response = restTemplate.
+               postForEntity(postReservationUrl, request, String.class);
+         JsonNode json = objectMapper.readTree(response.getBody());
+         
+         log.info("Status code from car server:" +
+               response.getStatusCodeValue());
+         //TODO: finish when known api structure exists
+         return null;
+      }
 }
