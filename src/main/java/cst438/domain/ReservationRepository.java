a5df394f09ed14ee52e72f03ac640e22e9606184
ndorazio137 <ndorazio@csumb.edu>
2021-06-10 05:44:47 -0700

    added database to application.properties

--- src/main/java/cst438/domain/ReservationRepository.java
+++ src/main/java/cst438/domain/ReservationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Package, Long> {
-   List<Package> findByUser(String userId);
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+   List<Reservation> findByUserId(int userId);
 }
