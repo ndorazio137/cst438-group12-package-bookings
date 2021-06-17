9fada2fbad069822229efbea4f5bae3a9c1a5b62
kyleoakes <koakes@csumb.edu>
2021-06-17 06:03:32 -0700

    added missing firstName and lastName fields

--- src/main/java/cst438/domain/User.java
+++ src/main/java/cst438/domain/User.java
@@ -21,13 +21,26 @@ public class User {
    @Size(min=1, max=254)
    private String password;
    
+   @NotNull
+   @Size(min=1, max=254)
+   private String firstName;
+   
+   @NotNull
+   @Size(min=1, max=254)
+   private String lastName;
+   
    public User() {}
    
-   public User(String username, String password) {
+   
+   public User(@NotNull @Size(min = 1, max = 254) String username, @NotNull @Size(min = 1, max = 254) String password,
+         @NotNull @Size(min = 1, max = 254) String firstName, @NotNull @Size(min = 1, max = 254) String lastName) {
+      super();
       this.username = username;
       this.password = password;
+      this.firstName = firstName;
+      this.lastName = lastName;
    }
-   
+
    public String getUsername() {
       return username;
    }
@@ -44,14 +57,37 @@ public class User {
       this.password = password;
    }
    
+   public String getFirstName() {
+      return firstName;
+   }
+
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+
+   public String getLastName() {
+      return lastName;
+   }
+
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+
    @Override
    public int hashCode() {
       final int prime = 31;
       int result = 1;
+      result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+      result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
       result = prime * result + ((password == null) ? 0 : password.hashCode());
       result = prime * result + ((username == null) ? 0 : username.hashCode());
       return result;
    }
+
 
    @Override
    public boolean equals(Object obj) {
@@ -62,6 +98,16 @@ public class User {
       if (getClass() != obj.getClass())
          return false;
       User other = (User) obj;
+      if (firstName == null) {
+         if (other.firstName != null)
+            return false;
+      } else if (!firstName.equals(other.firstName))
+         return false;
+      if (lastName == null) {
+         if (other.lastName != null)
+            return false;
+      } else if (!lastName.equals(other.lastName))
+         return false;
       if (password == null) {
          if (other.password != null)
             return false;
@@ -74,12 +120,11 @@ public class User {
          return false;
       return true;
    }
-
+   
    @Override
    public String toString() {
-      return "User [username=" + username + "]";
+      return "User [username=" + username + ", firstName=" + firstName + ", lastName=" + lastName + "]";
    }
-
    
    
 }
